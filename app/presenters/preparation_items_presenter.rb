be3875f9d6e040572309087b0fd62fb54418b4ca
danielkbr <danielkbr89@gmail.com>
2024-02-15 22:31:49 -0300

    layout in product preparations screen

--- app/presenters/preparation_items_presenter.rb
+++ app/presenters/preparation_items_presenter.rb
@@ -1,27 +1,31 @@
 class PreparationItemsPresenter < SimpleDelegator
   def items
-    "<table class='table table-dark table-bordered table-striped' style='overflow-x: scroll;'>
-      <thead>
-        <tr>
-          <th class='text-center'>Quantidade</th>
-          <th class='text-center'>Nome</th>
-          <th class='text-center'>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        #{preparation_items.map do |e|
-          "<tr>
-            <td class='text-center'><span class='badge bg-light text-primary'>#{e.quantity} x</span></td>
-            <td class='text-center'><span class='badge bg-primary'>#{e.product.item_name} #{e.product.seller_sku}</span></td>
-            <td class='text-center'><span class='badge bg-#{e.kind == 'pending' ? 'warning text-dark' : 'success'}'>#{e.kind}</span></td>
-          </tr>"
-        end.join}
-      </tbody>
-      <tfoot>
-        <tr>
-          <th colspan='3'></th>
-        </tr>
-      </tfoot>
-    </table>".html_safe
+    if preparation_items.present?
+      "<table class='table table-dark table-bordered table-striped' style='overflow-y: scroll;'>
+        <thead>
+          <tr>
+            <th class='text-center'>Quantidade</th>
+            <th class='text-center'>Nome</th>
+            <th class='text-center'>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          #{preparation_items.map do |e|
+            "<tr>
+              <td class='text-center'><span class='badge bg-light text-primary'>#{e.quantity} x</span></td>
+              <td class='text-center'><span class=''>#{e.product.item_name} #{e.product.seller_sku}</span></td>
+              <td class='text-center'><span class='badge bg-#{e.kind == 'pending' ? 'warning text-dark' : 'success'}'>#{e.kind}</span></td>
+            </tr>"
+          end.join}
+        </tbody>
+        <tfoot>
+          <tr>
+            <th colspan='3'></th>
+          </tr>
+        </tfoot>
+      </table>".html_safe
+    else
+      "<div class='alert alert-dark'> Sem itens de preparação adicionados </div".html_safe
+    end
   end
 end
