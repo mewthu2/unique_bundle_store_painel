45c702f641111f91d17f01ead3d23479de54c71c
danielkbr <danielkbr89@gmail.com>
2024-03-03 15:39:19 -0300

    open in next window orders

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,11 +18,10 @@ module ApplicationHelper
 
     diferenca_em_horas = ((Time.now - data_time) / 3600).round
 
-    # Se a diferença for menor que 24 horas, retorna quantas horas atrás
     if diferenca_em_horas < 24
       "#{diferenca_em_horas} hours ago"
     else
-      # Caso contrário, retorna quantos dias atrás
+
       dias_atras = (Time.now.to_date - data_time.to_date).to_i
       "#{dias_atras} days ago"
     end
