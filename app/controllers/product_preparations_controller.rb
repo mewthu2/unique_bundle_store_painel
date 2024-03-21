9a1885b47a245451804721f227cbf17d63e8e090
Jesher Minelli <jesherdevsk8@gmail.com>
2024-03-20 21:58:41 -0300

    add checkbox to preparate products

--- app/controllers/product_preparations_controller.rb
+++ app/controllers/product_preparations_controller.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'barby'
 require 'barby/barcode/code_128'
 require 'barby/outputter/png_outputter'
 
 class ProductPreparationsController < ApplicationController
-  before_action :set_product_preparations, only: [:edit, :update, :destroy]
-  before_action :load_references, only: [:new, :edit]
+  before_action :set_product_preparations, only: %i[edit update destroy]
+  before_action :load_references, only: %i[new edit]
   # GET /product_preparations
   def index
     @product_preparations = ProductPreparation.all
@@ -29,6 +31,32 @@ class ProductPreparationsController < ApplicationController
       redirect_to product_preparations_path, notice: 'Preparação de Produto criado com sucesso.'
     else
       render :new
+    end
+  end
+
+  def create_product_preparations
+    product_preparation_attributes = [
+      {
+        description: load_product_preparations.map do |preparation|
+                       "AmazonOrderID: #{preparation[:amazon_order_id]}"
+                     end.join(', '),
+        preparation_items_attributes: load_product_preparations.map do |preparation|
+          {
+            product_id: preparation[:product_id],
+            quantity: preparation[:number_of_items_unshipped],
+            kind: 'pending'
+          }
+        end
+      }
+    ]
+
+    ActiveRecord::Base.transaction do
+      ProductPreparation.create!(product_preparation_attributes)
+
+      permitted_params.each do |param|
+        OrderMarkup.find_or_create_by!(amazon_order_id: param[:amazon_order_id])
+                   .update(status: permitted_params.first[:order_status])
+      end
     end
   end
 
@@ -74,7 +102,8 @@ class ProductPreparationsController < ApplicationController
 
     respond_to do |format|
       format.pdf do
-        render pdf: 'generate_fnsku_tag', page_height: 30, page_width: 80, outline: { outline: false }, margin: { top: 1, bottom: 0, left: 1, right: 1 }
+        render pdf: 'generate_fnsku_tag', page_height: 30, page_width: 80, outline: { outline: false },
+               margin: { top: 1, bottom: 0, left: 1, right: 1 }
       end
     end
   end
@@ -90,19 +119,33 @@ class ProductPreparationsController < ApplicationController
     @product_preparation = ProductPreparation.find(params[:id])
   end
 
+  def load_product_preparations
+    @load_product_preparations ||= permitted_params.map do |order|
+      product = OrderItem.find_by(amazon_order_id: order[:amazon_order_id])&.product
+      product_id = product&.id
+
+      order.merge({ product_id: product_id })
+    end
+  end
+
+  def permitted_params
+    permit_params = params.permit(selected_orders: %I[amazon_order_id number_of_items_unshipped order_status])
+    permit_params[:selected_orders].values.map(&:to_h)
+  end
+
   # It allows only useful parameters.
   def product_preparations_params
     params.require(:product_preparation).permit(
       :description,
       :_destroy,
-      preparation_items_attributes: [
-        :id,
-        :product_id,
-        :quantity,
-        :kind,
-        :created_at,
-        :updated_at,
-        :_destroy
+      preparation_items_attributes: %i[
+        id
+        product_id
+        quantity
+        kind
+        created_at
+        updated_at
+        _destroy
       ]
     )
   end
