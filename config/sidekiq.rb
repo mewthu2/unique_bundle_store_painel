3ab4abdae2191a615366274023d8b501f8fdb742
danielkbr <danielkbr89@gmail.com>
2024-05-03 00:00:29 -0300

    fix: add redis url

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -1,3 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  if Rails.env.development?
+    config.redis = { url: 'redis://localhost:6379/0' }
+  elsif Rails.env.production?
+    config.redis = { url: ENV['REDIS_URL'] }
+  end
 end
