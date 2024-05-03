e7b006e97cf6ac535409c929114f9aff025b6c9f
danielkbr <danielkbr89@gmail.com>
2024-05-03 05:01:08 -0300

    feat: add new rediscloud url and remove node modules

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -2,6 +2,6 @@ Sidekiq.configure_server do |config|
   if Rails.env.development?
     config.redis = { url: 'redis://localhost:6379/0' }
   elsif Rails.env.production?
-    config.redis = { url: ENV['REDIS_URL'] }
+    config.redis = { url: ENV['REDISCLOUD_URL'] }
   end
 end
